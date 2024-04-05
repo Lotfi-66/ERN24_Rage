c99e171f8d07dc822c46d9b8631d6221a1e0b6ec
Format_Pro <lotfi.djeg@gmail.com>
2024-04-05 13:55:27 +0200

    A

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //on recupere notre div square
-let square = document.getElementById('square')
+let square = document.getElementById('square');
 
 //on va définir le style du carré
 //width :100px
+square.style.width = '100px';
 //height : 100px
+square.style.height = '100px';
 //background-color : red
+square.style.backgroundColor = 'red';
 //position : absolute
-//text-align : center
+square.style.position = 'center';
 //line-height : 100px
+square.style.lineHeight = '100px';
 //text => clique moi !
+square.style.textAlign = 'center';
+//text-align : center
+square.style.cursor = 'pointer';
+square.innerText = 'Clique moi !'
