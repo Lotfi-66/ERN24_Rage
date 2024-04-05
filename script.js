3f86d5743e304cf48d5201dd9a872793e7258fd6
Format_Pro <lotfi.djeg@gmail.com>
2024-04-05 13:46:22 +0200

    A

---
+++ script.js
@@ -0,0 +1,11 @@
+//on recupere notre div square
+let square = document.getElementById('square')
+
+//on va définir le style du carré
+//width :100px
+//height : 100px
+//background-color : red
+//position : absolute
+//text-align : center
+//line-height : 100px
+//text => clique moi !
